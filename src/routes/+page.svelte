33c894c50c42b1174ae41f97059882bf4b5003b3
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-17 12:48:20 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,29 @@
 <img
-	src="/lars.jpg"
-	alt="Le Vinh Khang"
-	width="100"
-	height="100"
-	class="mask-squircle mx-auto block mb-18"
+    src="/lars.jpg"
+    alt="Le Vinh Khang"
+    width="100"
+    height="100"
+    class="mask-squircle mx-auto block mb-18 fade-in"
 />
 
-<h1 class="font-semibold text-2xl mb-1 text-slate-900">Le Vinh Khang</h1>
-<h2 class="font-medium text-slate-500 mb-8">Web Developer</h2>
+<h1 class="font-semibold text-2xl mb-1 text-slate-900 fade-in">Le Vinh Khang</h1>
+<h2 class="font-medium text-slate-500 mb-8 fade-in">Web Developer</h2>
 
-<p class="font-medium mb-10 text-slate-800 text-lg">
-	I help companies build performant and scalable solutions for customer facing web apps.
+<p class="font-medium mb-10 text-slate-800 text-lg fade-in">
+    I help companies build performant and scalable solutions for customer facing web apps.
 </p>
 
-<ul class="flex font-medium text-slate-500 justify-center gap-6">
-	<li>
-		<a href="https://twitter.com/aedotris" class="hover:underline underline-offset-4">X</a
-		>
-	</li>
-	<li>
-		<a href="https://www.facebook.com/profile.php?id=199717642" class="hover:underline underline-offset-4"
-			>Facebook</a
-		>
-	</li>
-	<li>
-		<a
-			href="https://instagram.com/williamcachamwri"
-			class="hover:underline underline-offset-4">Instagram</a
-		>
-	</li>
-	<li>
-		<a href="https://github.com/levinhkhangzz" class="hover:underline underline-offset-4">Github</a>
-	</li>
+<ul class="flex font-medium text-slate-500 justify-center gap-6 fade-in">
+    <li>
+        <a href="https://twitter.com/aedotris" class="hover:underline underline-offset-4">X</a>
+    </li>
+    <li>
+        <a href="https://www.facebook.com/profile.php?id=199717642" class="hover:underline underline-offset-4">Facebook</a>
+    </li>
+    <li>
+        <a href="https://instagram.com/williamcachamwri" class="hover:underline underline-offset-4">Instagram</a>
+    </li>
+    <li>
+        <a href="https://github.com/levinhkhangzz" class="hover:underline underline-offset-4">Github</a>
+    </li>
 </ul>
