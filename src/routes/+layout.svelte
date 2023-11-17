6c1c84db1f851514864cfead02ede6bd85fd5c14
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-17 12:36:15 +0000

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,6 @@
 <script>
-  import { onMount } from 'svelte';
   import Loading from './Loading.svelte';
   import '../app.css';
-
-  let isLoading = true; // Bắt đầu với trạng thái loading là true
-
-  // Hàm để tắt phần loading
-  function hideLoading() {
-    isLoading = false;
-  }
-
-  // Sử dụng onMount để thiết lập hẹn giờ
-  onMount(() => {
-    setTimeout(hideLoading, 500); // Tắt phần loading sau 1.5 giây (1500ms)
-  });
 </script>
 
 <svelte:head>
@@ -25,12 +12,6 @@
 </svelte:head>
 
 <main>
-  {#if isLoading}
-    <Loading /> <!-- Hiển thị phần loading khi isLoading là true -->
-  {/if}
-
-  <!-- Nội dung trang web -->
-  <!-- Đây là nơi bạn đặt nội dung thực tế của trang web -->
   <div class="w-full pt-[20vh] pb-16">
     <div class="w-full px-4 max-w-md mx-auto text-center">
       <slot />
