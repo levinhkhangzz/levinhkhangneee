5a99587fb9c6b761caff0fb8f8fbc652bc7422df
levinhkhangzz <levinhkhang631@gmail.com>
2023-11-17 11:07:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,16 @@
+# larsgraubner.com
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/74c4d0c0-8cc9-4107-b6cb-046b2d027918/deploy-status)](https://app.netlify.com/sites/friendly-dubinsky-06aa6c/deploys)
+
+Personal website of Lars Graubner.
+
+## Getting started
+
+```
+# start local development
+pnpm install
+pnpm run dev
+
+# build website
+pnpm run build
+```
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Lars Graubner – Web Developer</title>
+	<meta
+		name="description"
+		content="I help companies build performant and scalable solutions for customer facing web apps"
+	/>
+</svelte:head>
+
+<main>
+	<div class="w-full pt-[20vh] pb-16">
+		<div class="w-full px-4 max-w-md mx-auto text-center">
+			<slot />
+		</div>
+	</div>
+</main>
