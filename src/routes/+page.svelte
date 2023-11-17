94fe6cae8d27c2b1484a72fe3e2f510e30223f90
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-17 14:03:34 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,10 @@
     alt="Le Vinh Khang"
     width="150"
     height="150"
-    class="mask-squircle mx-auto block mb-9 fade-in"
+    class="mask-squircle mx-auto block mb-0 fade-in"
 />
 
-<h1 class="font-semibold text-2xl mb-5 text-slate-900 fade-in">Le Vinh Khang</h1>
+<h1 class="font-semibold text-2xl mb-1 text-slate-900 fade-in">Le Vinh Khang</h1>
 <h2 class="font-medium text-slate-500 mb-10 fade-in">Web Developer</h2>
 
 <p class="font-medium mb-10 text-slate-800 text-lg fade-in">
