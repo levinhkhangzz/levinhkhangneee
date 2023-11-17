ad2cdfa1c4aff9988ae12cbe3a952f94b0331685
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-17 12:43:00 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,35 @@
-<!-- Trang Svelte -->
-<script>
-  import { onMount } from 'svelte';
-
-  let isLoaded = false;
-
-  // Sử dụng onMount để thực hiện việc xác định khi trang đã tải
-  onMount(() => {
-    isLoaded = true;
-  });
-
-  // Hàm xử lý khi hoàn thành hiệu ứng
-  function onAnimationEnd() {
-    // Ẩn phần tử gốc sau khi hiệu ứng hoàn thành (nếu cần)
-  }
-</script>
-
-<style>
-  /* Bắt đầu với opacity: 0 */
-  .fade-in {
-    opacity: 0;
-  }
-</style>
-
-<!-- Sử dụng lớp animate__animated và animate__fadeIn để áp dụng hiệu ứng fade-in -->
 <img
-  src="/lars.jpg"
-  alt="Le Vinh Khang"
-  width="100"
-  height="100"
-  class="{isLoaded ? 'animate__animated animate__fadeIn fade-in' : 'fade-in'}"
-  on:animationend={onAnimationEnd}
+	src="/lars.jpg"
+	alt="Le Vinh Khang"
+	width="100"
+	height="100"
+	class="mask-squircle mx-auto block mb-18"
 />
 
-<h1
-  class="{isLoaded ? 'animate__animated animate__fadeIn fade-in' : 'fade-in'}"
-  on:animationend={onAnimationEnd}
->
-  Le Vinh Khang
-</h1>
-<h2
-  class="{isLoaded ? 'animate__animated animate__fadeIn fade-in' : 'fade-in'}"
-  on:animationend={onAnimationEnd}
->
-  Web Developer
-</h2>
+<h1 class="font-semibold text-2xl mb-1 text-slate-900">Le Vinh Khang</h1>
+<h2 class="font-medium text-slate-500 mb-8">Web Developer</h2>
 
-<p
-  class="{isLoaded ? 'animate__animated animate__fadeIn fade-in' : 'fade-in'}"
-  on:animationend={onAnimationEnd}
->
-  I help companies build performant and scalable solutions for customer facing web apps.
+<p class="font-medium mb-10 text-slate-800 text-lg">
+	I help companies build performant and scalable solutions for customer facing web apps.
 </p>
 
-<!-- Sử dụng lớp fade-in và điều kiện isLoaded để áp dụng hiệu ứng fade-in -->
-<ul class="flex font-medium text-slate-500 justify-center gap-6 {isLoaded ? 'animate__animated animate__fadeIn fade-in' : 'fade-in'}">
-  <li>
-    <a href="https://twitter.com/aedotris" class="hover:underline underline-offset-4">X</a>
-  </li>
-  <li>
-    <a href="https://www.facebook.com/profile.php?id=199717642" class="hover:underline underline-offset-4">Facebook</a>
-  </li>
-  <li>
-    <a href="https://instagram.com/williamcachamwri" class="hover:underline underline-offset-4">Instagram</a>
-  </li>
-  <li>
-    <a href="https://github.com/levinhkhangzz" class="hover:underline underline-offset-4">Github</a>
-  </li>
+<ul class="flex font-medium text-slate-500 justify-center gap-6">
+	<li>
+		<a href="https://twitter.com/aedotris" class="hover:underline underline-offset-4">X</a
+		>
+	</li>
+	<li>
+		<a href="https://www.facebook.com/profile.php?id=199717642" class="hover:underline underline-offset-4"
+			>Facebook</a
+		>
+	</li>
+	<li>
+		<a
+			href="https://instagram.com/williamcachamwri"
+			class="hover:underline underline-offset-4">Instagram</a
+		>
+	</li>
+	<li>
+		<a href="https://github.com/levinhkhangzz" class="hover:underline underline-offset-4">Github</a>
+	</li>
 </ul>
