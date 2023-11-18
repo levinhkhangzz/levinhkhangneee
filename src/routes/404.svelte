f14c6c531736ee25dd19b9dad62c9713ad7b65ae
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-18 00:19:25 +0000

    Create 404.svelte

---
+++ src/routes/404.svelte
@@ -0,0 +1,16 @@
+<!-- 404.svelte -->
+<script>
+  export let error;
+</script>
+
+<main>
+  <h1>Trang không tồn tại</h1>
+  <p>{error.message}</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2em;
+  }
+</style>
