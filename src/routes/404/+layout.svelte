bc2bd6968338fdc473ec9eb2947a21dc83ebd2c5
Aedotris ⛧ <127677696+levinhkhangzz@users.noreply.github.com>
2023-11-18 00:35:45 +0000

    Update +layout.svelte

--- src/routes/404/+layout.svelte
+++ src/routes/404/+layout.svelte
@@ -1,1 +1,15 @@
+<!-- 404/+layout.svelte -->
+<script context="module">
+  export { default } from '$layout';
+</script>
 
+<main>
+  <slot></slot>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2em;
+  }
+</style>
